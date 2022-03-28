a1d5ebde69265fcf3211a9f59f0592517613e174
Sagar Kharab <sagar.kharab@anaplan.com>
2022-03-28 09:19:10 +0100

    INTEGRA-2098 Restructuring

--- java/anaplan-connect-client/src/test/java/com/anaplan/client/integrationTests/helpers/constants/IntegrationConstants.java
+++
@@ -1,9 +0,0 @@
-package com.anaplan.client.integrationTests.helpers.constants;
-
-public interface IntegrationConstants {
-
-  String SCRIPTS_FOLDER = "integration-tests/scripts";
-  String EXPORT_FOLDER = "/export";
-  String DUMP_FOLDER = "/dump";
-
-}
