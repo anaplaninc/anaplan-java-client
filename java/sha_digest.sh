e3acacc76af03703f4cd0a7051e0181af763a0c5
Arman Sargsyan <arman.sargsyan@anaplan.com>
2022-10-05 13:32:00 +0100

    : added

--- java/sha_digest.sh
+++ java/sha_digest.sh
@@ -6,6 +6,5 @@ FILE_NAME=sha-digest-version.txt
 CURRENT_DATE_TIME=$(date '+%Y-%m-%dT%T.%zZ')
 SHA_256_VALUE=$(cd anaplan-connect-client/target; openssl dgst -sha256 anaplan-connect-client-${VERSION}-jar-with-dependencies.jar | cut -d=  -f2)
 
-RECORD="DATE ${CURRENT_DATE_TIME} | Version ${VERSION} | SHA digestID ${SHA_256_VALUE}"
+RECORD="DATE ${CURRENT_DATE_TIME} | Version ${VERSION} | SHA digestID: ${SHA_256_VALUE}"
 echo "$(echo ${RECORD}; cat ${FILE_NAME})" > $FILE_NAME
-
