218e38c57585d70e4a2be2c2f36196b26ef0f645
Arman Sargsyan <arman.sargsyan@anaplan.com>
2022-10-05 13:27:38 +0100

    sha digest version added for jar builds

---
+++ java/sha_digest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+VERSION="3.0.0"
+
+
+FILE_NAME=sha-digest-version.txt
+CURRENT_DATE_TIME=$(date '+%Y-%m-%dT%T.%zZ')
+SHA_256_VALUE=$(cd anaplan-connect-client/target; openssl dgst -sha256 anaplan-connect-client-${VERSION}-jar-with-dependencies.jar | cut -d=  -f2)
+
+RECORD="DATE ${CURRENT_DATE_TIME} | Version ${VERSION} | SHA digestID ${SHA_256_VALUE}"
+echo "$(echo ${RECORD}; cat ${FILE_NAME})" > $FILE_NAME
+
